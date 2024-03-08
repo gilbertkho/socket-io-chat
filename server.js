b44e35cb566a39cde38810ba88f8d2981324cdd7
Gabriel Gilbert Khomaro <gilbertkho17@yahoo.com>
2024-03-08 17:47:55 +0700

    add read receipt

--- server.js
+++ server.js
@@ -14,30 +14,75 @@ const io = require('socket.io')(server,{
     },
 });
 
-io.on('connection', socket => {
+let connectedUsers = [];
+io.on('connection', async (socket) => {
+    //connectedUsers.push(socket);
+    //console.log(socket);
+    // const projects = await fetchProjects(socket);
+    // console.log(projects);
     //console.log(socket.id);
-    socket.on('send-message',(message, room) => {
+
+    socket.on('send-message', (message, room, name = "") => {
         //io.emit('receive-message',message);
         //socket.broadcast.emit("receive-message", message);
+        console.log('send-message', {
+            message: message,
+            room: room,
+            name: name,
+        });
+
         if(room === ''){
-            //console.log('MASUK SINI');
-            //broadcast message
-            socket.broadcast.emit("receive-message", message);
+            //broadcast message to all connected user
+            socket.broadcast.emit("receive-message", message, name);
         }
         else{
             //sending message privately to room
             //meet person to one room
-            socket.to(room).emit("receive-message", message);
+            socket.to(room).emit("receive-message", message, name);
+            getConnectedUsers(room, name);
         }
     });
 
+    const getConnectedUsers = (room, name) => {
+        let userData = {
+            socket_id: socket.id,
+            room: room,
+            name: name,
+        }
+
+        //console.log("CONNECTED USERS", userData);
+
+        let check_users = false //not exist
+        for(let i = 0; i < connectedUsers.length; i++){
+            if(connectedUsers[i].room && connectedUsers[i].room != ''){
+                if(connectedUsers[i].room  == room && connectedUsers[i].name == name){
+                    check_users = true;
+                }
+            }
+        }
+
+        if(!check_users){
+            connectedUsers.push(userData);
+        }
+        socket.to(room).emit('connected-users', connectedUsers);
+
+
+        //console.log(connectedUsers);
+    }
+
+    socket.on('read-receipt', (room, name = '') => {
+        getConnectedUsers(room, name);
+    });
+
     //join room
-    socket.on('join-room', (room) => {
+    socket.on('join-room', (room, name = '') => {
+        console.log(room, name);
         socket.join(room);
-    })
+        getConnectedUsers(room, name);
+    });
 
     //typing status
-    socket.on('typing', (room, status) => {
+    socket.on('typing', (room, name = '', status) => {
         console.log(room,status);
         if(room === ''){
             //console.log('MASUK SINI');
@@ -49,6 +94,34 @@ io.on('connection', socket => {
             //meet person to one room
             socket.to(room).emit("type-status", status);
         }
+        getConnectedUsers(room, name);
+    })
+
+    socket.on('disconnect', () => {
+        //console.log('JUMLAH ROOM DISCONNECT',socket.client);
+        console.log(socket.id);
+        if(connectedUsers.length > 0){
+            let check_users = false;
+            for(let i = 0; i < connectedUsers.length; i++){
+                if(connectedUsers[i].socket_id == socket.id){
+                    check_users = true;
+                }
+            }
+
+            console.log('USER ARRAY', connectedUsers);
+    
+            let getDisconnectedIdx = connectedUsers.findIndex((con) => con.socket_id == socket.id);
+            console.log('SOCKET ID ARRAY INDEX',getDisconnectedIdx);
+            let room               = connectedUsers[getDisconnectedIdx].room;
+    
+            connectedUsers.splice(getDisconnectedIdx, 1);
+            console.log('USERS LEFT : ',connectedUsers);
+            socket.to(room).emit('disconnected-users', connectedUsers);
+        }
+        
+        //getConnectedUsers();
+        //var i = connectedUsers.indexOf(socket);
+        //connectedUsers.splice(i,1);
     })
 });
 
--- src/script.js
+++ src/script.js
@@ -1,29 +1,49 @@
 const socket = io('http://localhost:8080');
 
-const showMessage = (message, info) => {
+let connectedUsers = 0;
+
+const showMessage = (message, name = '', info) => {
     let bubble = info ==  'client' ? 
-    `<div class="msg flex justify-end text-black p-[10px] bg-slate-500 ">
-            ${message}
+    `<div class="msg flex justify-end text-black">
+        <div class="w-fit border border-black p-[10px] m-[15px]">
+            ${name}
+            <ul class="list-disc list-inside">
+                <li>${message}</li>
+            </ul>
+        </div>
     </div>`: 
-    `<div class="msg flex justify-start text-black p-[10px] bg-slate-500 ">
-            ${message}
+    `<div class="msg flex justify-start text-black">
+        <div class="w-fit border border-black p-[10px] m-[15px]">
+            ${name}
+            <ul class="msg-content list-disc list-inside ${connectedUsers > 1 ? 'read-receipt' : ''}">
+                <li>${message}</li>
+            </ul>
+        </div>
     </div>`;
 
     $('#chat-content').append(bubble);
+    var elem = document.getElementById('chat-content');
+    elem.scrollTop = elem.scrollHeight;
 }
 
 const showTypingStatus = (status, info) => {
-    console.log('brooo ajaa');
+    //console.log('brooo ajaa');
     let bubble = info ==  'client' ? 
-    `<div class="typing flex justify-end text-black p-[10px] bg-slate-500 ">
-        <img src="./typing.gif" width='100'>
+    `<div class="typing flex justify-end text-black">
+        <div class="w-fit border border-black p-[10px] m-[15px]">
+            <img src="./typing.gif" width='100'>
+        </div>
     </div>`: 
-    `<div class="typing flex justify-start text-black p-[10px] bg-slate-500 ">
-        <img src="./typing.gif" width='100'>
+    `<div class="typing flex justify-start text-black">
+        <div class="w-fit border border-black p-[10px] m-[15px]">
+            <img src="./typing.gif" width='100'>
+        </div>
     </div>`;
 
     if(status && $('.typing').length <= 0){
         $('#chat-content').append(bubble);
+        var elem = document.getElementById('chat-content');
+        elem.scrollTop = elem.scrollHeight;
     }
     else if(!status){
         $('.typing').remove();
@@ -31,27 +51,30 @@ const showTypingStatus = (status, info) => {
 }
 
 $("#join").on('click', function(){
+    let name    = $('#name').val() ? $('#name').val() : '';
     let room    = $('#room').val();
-    socket.emit('join-room', room);
+    //console.log(room, name);
+    socket.emit('join-room', room, name);
 });
 
-document.getElementById('send').addEventListener('click', function(){
-    let message = document.getElementById('message').value;
+$('#send').on('click', function(){
+    let name    = $('#name').val() ? $('#name').val() : '';
+    let message = $('#message').val();
     let room    = $('#room').val();
     if(!message) return;
-    showMessage(message, 'own');
-    socket.emit("send-message", message, room);
-    document.getElementById('message').value = '';
-    //console.log(socket);
+    showMessage(message, name, 'own');
+    socket.emit("send-message", message, room, name);
+    $('#message').val('');
 });
 
 const typing = (info) => {
+    let name    = $('#name').val() ? $('#name').val() : '';
     let room    = $('#room').val();
     if(info === 'still'){
-        socket.emit("typing", room, true);
+        socket.emit("typing", room, name, true);
     }
     else if(info ===  'done'){
-        socket.emit("typing", room, false);
+        socket.emit("typing", room, name, false);
     }
 }
 
@@ -59,7 +82,7 @@ let typingInterval;
 
 $("#message").on('keyup', function(){
     typing('still');
-    console.log('key upp bro');
+    //console.log('key upp bro');
     clearTimeout(typingInterval);
     typingInterval = setTimeout(() => {
         typing('done');
@@ -67,15 +90,37 @@ $("#message").on('keyup', function(){
 });
 
 socket.on('connect', () => {
-    showMessage(`You connected with id ${socket.id}`);
+    //showMessage(`You connected with id ${socket.id}`);
 });
 
-socket.on('receive-message', message => {
-    showMessage(message, 'client');
-    document.getElementById('#chat-content').scrollTo(0);
+//get message
+socket.on('receive-message', (message, name) => {
+    //console.log(message, name);
+    showMessage(message, name, 'client');
+    console.log('users connected: ',connectedUsers);
+    //showMessage(message, name, 'client');
+    //document.getElementById('#chat-content').scrollTo(0);
 });
 
+//send type status
 socket.on('type-status', (status) => {
     showTypingStatus(status, 'client');
-    console.log('tesss');
+    //console.log('tesss');
 });
+
+//get connected users
+socket.on('connected-users', (users) => {
+    if(users.length > 0){
+        $(".msg-content").addClass('read-receipt');
+        let name    = $('#name').val() ? $('#name').val() : '';
+        let room    = $('#room').val();
+        socket.emit('read-receipt', room, name);
+        connectedUsers = users.length;
+    }
+    //console.log(users);
+});
+
+socket.on('disconnected-users', (users) => {
+    connectedUsers = users.length;
+    //console.log(users);
+})
